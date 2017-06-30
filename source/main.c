6193b38bd50d725c570378651567d9d6cf778d13
LiquidFenrir <liquidfenrir@outlook.fr>
2017-06-30 03:01:03 +0200

    features ?

--- source/main.c
+++ source/main.c
@@ -23,6 +23,7 @@ int main()
 	
 	consoleSelect(&debugConsole);
 	Result ret = initComm(server);
+	//if an error happened during the connection, just quit the application
 	if (ret != 0) goto exit;
 	
 	printInstructions();
@@ -69,7 +70,7 @@ int main()
 			filePacket packet = {0};
 			
 			u32 requestedBlock = 0;
-			u32 previousBlock = 0;
+			u32 previousBlock = 1; //placeholder value, will get set to 0 afterwards
 			u64 offset = 0;
 			
 			closeFile(filehandle);
@@ -77,33 +78,46 @@ int main()
 			
 			printf("Starting sending, press B to abort...\n");
 			while (true) {
+				
+				hidScanInput();
+				if ((hidKeysDown() | hidKeysHeld()) & KEY_B) break;
+				
 				//receive the id of the block to send
 				do {
 					receiveData(&requestedBlock, sizeof(requestedBlock), &receivedSize);
+					hidScanInput();
+					if ((hidKeysDown() | hidKeysHeld()) & KEY_B) break;
 				} while(receivedSize == 0);
 				
 				//if the received data is "OK" followed by a nullbyte, the receiver is done and we can stop
 				if (receivedSize == 3 && !strncmp((char*)&requestedBlock, "OK", 3)) break;
 				
-				hidScanInput();
-				if (hidKeysDown() & KEY_B) {
-					//tell the receiver we stop sending and he can abort
-					sendString("NO");
-					break;
-				}
-				
 				//if the requested block id increased, the data was received correctly and we can carry on
 				if (requestedBlock != previousBlock) offset += packet.size;
+				else {
+					//if the requested id didn't change, send the same data, no need to read from the file again
+					sendData(&packet, sizeof(packet));
+					continue;
+				}
 				
 				packet.packetID = requestedBlock;
 				readFile(filehandle, &packet.size, offset, packet.data, PACKET_DATA_SIZE);
 				packet.checksum = getBufChecksum(packet.data, packet.size);
 				
-				sendData(&packet, PACKET_DATA_SIZE);
+				sendData(&packet, sizeof(packet));
 				
 				previousBlock = requestedBlock;
 				
 				progressBar(requestedBlock, blockCount);
+			}
+			printf("\n"); //to skip the progress bar
+			
+			if (receivedSize == 3 && !strncmp((char*)&requestedBlock, "OK", 3))
+				printf("Done sending!");
+			else {
+				printf("Transfer aborted.\n");
+				//tell the receiver we stop sending and he can abort
+				sendString("NO");
 			}
 			
 			closeFile(filehandle);
@@ -148,8 +162,9 @@ int main()
 			
 			printf("Starting requests, press B to abort...\n");
 			for (i = 0; i < blockCount; i++) {
+				
 				hidScanInput();
-				if (hidKeysDown() & KEY_B) break;
+				if ((hidKeysDown() | hidKeysHeld()) & KEY_B) break;
 				
 				//send the id of the block to receive
 				sendData(&i, sizeof(u32));
@@ -157,6 +172,8 @@ int main()
 				//wait until you receive the packet
 				do {
 					receiveData(&packet, sizeof(packet), &receivedSize);
+					hidScanInput();
+					if ((hidKeysDown() | hidKeysHeld()) & KEY_B) break;
 				} while(receivedSize == 0);
 				
 				//if the received data is "NO" followed by a nullbyte, the sender aborted
@@ -178,14 +195,11 @@ int main()
 			}
 			printf("\n"); //to skip the progress bar
 			
-			if (i == blockCount) {
-				printf("File completely received!\n");
-				//tell the sender to stop
-				sendString("OK");
-			}
-			else {
-				printf("Download aborted.\n");
-			}
+			//tell the sender to stop
+			sendString("OK");
+			
+			if (i == blockCount) printf("File completely received!\n");
+			else printf("Download aborted.\n");
 			
 			closeFile(filehandle);
 		}
